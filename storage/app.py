3fd06f215e23c6f45ffb4104288cfa8d0bffc21c
Jayant Parihar <jayantparihar@yahoo.com>
2022-11-16 11:55:40 -0800

    added code for log and app config

--- storage/app.py
+++ storage/app.py
@@ -15,10 +15,28 @@ from threading import Thread
 from pykafka.common import OffsetType
 from sqlalchemy import and_
 import time
+import os 
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-with open('app_conf.yml', 'r') as f:
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 user = app_config['datastore']['user']
@@ -34,13 +52,6 @@ Base.metadata.bind = DB_ENGINE
 Base.metadata.create_all(DB_ENGINE)
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
-
-    
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-logger = logging.getLogger('basicLogger')
 logger.debug("Connecting to database %s %s %s",hostname,port,db)
 
 
