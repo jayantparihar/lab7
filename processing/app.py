a82e7a58c8ff8f6d528cbc038e1f17790d9d6157
Jayant Parihar <jayantparihar@yahoo.com>
2022-11-21 11:48:00 -0800

    trying to fix processing

--- processing/app.py
+++ processing/app.py
@@ -109,17 +109,7 @@
 #         num_chlorine_level = len(chlorine_list)
 #         last_updated = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 #     session = DB_SESSION()
-#     # for i in ph_list:
-#     #     if i["phlevel"] >  max_phlevel_reading:
-#     #         max_phlevel_reading = i["phlevel"] 
-#     #     if i["waterlevel"] > max_water_level:
-#     #         max_water_level = i["waterlevel"] 
 
-#     # for i in chlorine_list:
-#     #   if i["chlorinelevel"] > max_chlorine_level:
-#     #     max_chlorine_level = i["chlorinelevel"]
-#     #   if i["waterlevel"] > max_water_level:
-#     #     max_water_level = i["waterlevel"] 
 #     session = DB_SESSION()
 #     stats = Stats(num_phlevel_reading,
 #         max_phlevel_reading,
@@ -284,6 +274,17 @@ def populate_stats():
     session = DB_SESSION()
     for i in ph_list:
         logger.debug(f"new event with a trace id of {i['trace_id']}")
+        if i["phlevel"] >  max_phlevel_reading:
+            max_phlevel_reading = i["phlevel"] 
+        if i["waterlevel"] > max_water_level:
+            max_water_level = i["waterlevel"] 
+
+    for i in chlorine_list:
+      if i["chlorinelevel"] > max_chlorine_level:
+        max_chlorine_level = i["chlorinelevel"]
+      if i["waterlevel"] > max_water_level:
+        max_water_level = i["waterlevel"] 
+
     session = DB_SESSION()
     stats = Stats(num_phlevel_reading,
         max_phlevel_reading,
