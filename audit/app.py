7ea11c5f9ecfe731712acefbe69310296267c852
Jayant Parihar <jayantparihar@yahoo.com>
2022-11-28 09:41:47 -0800

    lab 11 part 1

--- audit/app.py
+++ audit/app.py
@@ -108,9 +108,10 @@ def health_check():
     return dictionary, 200
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml")
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+app.add_api("openapi.yml", base_path="/audit", strict_validation=True,validate_responses=True)
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     
--- receiver/app.py
+++ receiver/app.py
@@ -103,9 +103,8 @@ def health_check():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",
-        strict_validation=True,
-        validate_responses=True)
+app.add_api("openapi.yml", base_path="/receiver", strict_validation=True,
+validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
